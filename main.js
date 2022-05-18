7117d972ffd8580adff0c15d8dc191025a5955f0
Vinicius Hasselmann <105175439+vinihasselmann@users.noreply.github.com>
2022-05-17 23:16:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'vinihasselmann',
+  youtube: 'naotemyoutube',
+  facebook: 'vinicius.hasselmann',
+  instagram: 'vinihasselmann',
+  twitter: 'hasselmannv'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    alert(li.children[0].href)
+  }
+}
+
+//changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userGH.textContent = data.login
+    })
+}
